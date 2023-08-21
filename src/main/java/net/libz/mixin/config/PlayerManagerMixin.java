a10e0c48621cec291efbd928161f928bc551b216
Globox1997 <mitschke11@web.de>
2023-08-21 12:09:58 +0200

    flush

--- src/main/java/net/libz/mixin/config/PlayerManagerMixin.java
+++ src/main/java/net/libz/mixin/config/PlayerManagerMixin.java
@@ -15,7 +15,6 @@ import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.libz.api.ConfigSync;
 import net.libz.init.ConfigInit;
 import net.libz.network.LibzServerPacket;
-import net.libz.util.ConfigHelper;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.registry.RegistryKey;
@@ -33,23 +32,6 @@ public class PlayerManagerMixin {
     @Inject(method = "onPlayerConnect", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/server/world/ServerWorld;getGameRules()Lnet/minecraft/world/GameRules;"), locals = LocalCapture.CAPTURE_FAILSOFT)
     private void onPlayerConnectMixin(ClientConnection connection, ServerPlayerEntity player, CallbackInfo info, GameProfile gameProfile, UserCache userCache, String string, NbtCompound nbtCompound,
             RegistryKey<World> registryKey, ServerWorld serverWorld, ServerWorld serverWorld2, String string2, WorldProperties worldProperties, ServerPlayNetworkHandler serverPlayNetworkHandler) {
-
-        // TEST
-        // AutoConfigAccess.getHolders().forEach((data, holder) -> {
-        //     if (holder.getConfig() instanceof ConfigSync) {
-        //         ConfigSerializer<?> configSerializer = ((ConfigManager<?>) holder).getSerializer();
-
-        //         if (configSerializer instanceof GsonConfigSerializer || configSerializer instanceof JanksonConfigSerializer) {
-
-        //             byte[] bytes = ConfigHelper.getConfigBytes(((ConfigManager<?>) holder).getDefinition().name(), configSerializer instanceof GsonConfigSerializer, true);
-        //             System.out.println(bytes + " : " + bytes.length);
-        //             for (int i = 0; i < bytes.length; i++) {
-        //                 // System.out.println(FileUtils.sizeOf(bytes[i]));
-        //                 System.out.println(i + ": " + bytes[i]);
-        //             }
-        //         }
-        //     }
-        // });
 
         if (ConfigInit.CONFIG.syncConfig) {
             if (!connection.isLocal()) {
